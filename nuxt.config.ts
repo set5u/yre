ec77da0ead3eca3441513a748216d6a7ea6c0072
ajiken4610 <ajiken4610@gmail.com>
2025-03-03 16:03:39 +0900

    save

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,30 @@
+import swc from "unplugin-swc";
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
   imports: { autoImport: false },
   ssr: false,
+  modules: ["@vueuse/nuxt", "@nuxtjs/tailwindcss"],
+  $production: {
+    vite: {
+      plugins: [
+        swc.rollup({
+          // @ts-ignore
+          exclude: [/core-js/, /howler/],
+          tsconfigFile: false,
+          jsc: {
+            parser: {
+              syntax: "typescript",
+            },
+          },
+          env: {
+            coreJs: "3.41.0",
+            mode: "usage",
+            forceAllTransforms: true,
+          },
+        }),
+      ],
+    },
+  },
 });
